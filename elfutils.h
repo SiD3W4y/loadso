cb46a2f2c2f0689471f239e53dfd9a8bcf49d6ac
Sideway <dev.sideway@gmail.com>
2018-07-09 12:41:16 +0200

    Added macros for linux/android separation and 32/64 bits handling skeleton

--- elfutils.h
+++ elfutils.h
@@ -3,6 +3,25 @@
 
 #include <elf.h>
 
+#ifdef __ANDROID_API__
+    #ifdef __aarch64__
+        #define TARGET_PATTERN "/system/bin/linker64"
+        #define TARGET_SYMBOL "__dl_dlopen"
+    #else
+        #define TARGET_PATTERN "/system/bin/linker"
+        #define TARGET_SYMBOL "__dl_dlopen"
+    #endif
+#else
+    #ifdef __x86_64__
+        #define TARGET_PATTERN "libc-"
+        #define TARGET_SYMBOL "__libc_dlopen_mode"
+    #else
+        #define TARGET_PATTERN "libc-"
+        #define TARGET_SYMBOL "__libc_dlopen_mode"
+    #endif
+#endif
+
+
 Elf64_Shdr *elf64_getsection(void *elfbase, char *name);
 Elf32_Shdr *elf32_getsection(void *elfbase, char *name);
 
--- loadso.c
+++ loadso.c
@@ -41,13 +41,62 @@ static long __module_base(int pid, char *name)
     char *line;
 
     while((line = fgets(buff, 512, fp)) != NULL) {
-        printf(">> %s", line);
         if(strstr(line, name) > 0) {
             return strtoul(line, NULL, 16);
         }
     }
 
     return 0;
+}
+
+static char *__module_name(int pid, char *pattern)
+{
+    FILE *fp;
+    char buff[512];
+
+    if(pid == 0) {
+        fp = fopen("/proc/self/maps", "r");
+    } else {
+        snprintf(buff, 512, "/proc/%u/maps", pid);
+        fp = fopen(buff, "r");
+    }
+
+    if(fp == NULL)
+        return NULL;
+
+    char *line;
+
+    while((line = fgets(buff, 512, fp)) != NULL) {
+        if(strstr(line, pattern) > 0) {
+            int i = strlen(line);
+            line[i-1] = '\0';
+            
+            while(i > 0 && line[i] != ' ') {
+                i--;
+            }
+            
+            return strdup(&line[i+1]);
+        }
+    }
+
+    return NULL;
+}
+
+static int __module_bits(void *mapped_elf)
+{
+    Elf64_Ehdr *header = (Elf64_Ehdr *)mapped_elf;
+
+    switch(header->e_ident[EI_CLASS]) {
+        case ELFCLASS32:
+            return 32;
+            break;
+        case ELFCLASS64:
+            return 64;
+            break;
+        case ELFCLASSNONE:
+        default:
+            return 0;
+    }
 }
 
 loadso_err loadso_inject(int pid, char *path)
@@ -64,8 +113,31 @@ loadso_err loadso_inject(int pid, char *path)
     
     printf("[+] Injecting object \"%s\" in target process %d\n", path, pid);
     
-    // We compute relative offsets for the functions
-    elf64_getsection(__map_file("/system/bin/linker64"), ".strtab");
+    // Now loading the right module in memory
+    char *module_name = __module_name(pid, TARGET_PATTERN);
+
+    if(module_name == NULL)
+        return loadso_err_file; // loadso_err_module
+
+    printf("[+] Using function \"" TARGET_SYMBOL "\" from lib %s\n", module_name);
+    void *module_data = __map_file(module_name);
+
+    int bits = __module_bits(module_data);
+    
+    if(bits == 0)
+        return loadso_err_file; // loadso_err_arch
+
+    // now that we have the bits we can call the right functions
+    long module_base = __module_base(pid, module_name);
+
+    if(bits == 32) {
+        printf("[+] 32bits\n");
+    } else {
+        printf("[+] 64bits\n"); 
+    }
+
+    tracer_detach(pid);
+    
     return loadso_err_ok; 
 }
 
